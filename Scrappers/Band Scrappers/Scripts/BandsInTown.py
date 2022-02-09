9f0ac2d5af1490908d32b501ac500cf861baeac6
Nate Hedgeman <47337337+hedgemanNate@users.noreply.github.com>
2022-02-09 16:27:44 -0500

    added hula bay club to google calendar scrapper

---
+++ Scrappers/Band Scrappers/Scripts/BandsInTown.py
@@ -0,0 +1,114 @@
+# %%
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup as BS
+
+import time as time
+from dateutil.parser import parse
+from dateutil.relativedelta import relativedelta
+import datetime
+import os
+import json
+
+from OhmicityShared import ohmicity_shared
+
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# %%
+
+'''
+LIST OF BANDS:
+
+1: Selwyn Birchwood
+2: Jah Movement
+3: Ariella
+4: Sarah Diamond
+5: 
+'''
+
+urls = [
+    'https://www.bandsintown.com/a/8596214-selwyn-birchwood-band?', 'https://www.bandsintown.com/a/4009595-jah-movement?', 'https://www.bandsintown.com/a/2470649', 'https://www.bandsintown.com/a/1710060-ariella?',
+    
+]
+
+band_name = '****************'
+
+# %%
+options = Options()
+options.headless = False
+driver = webdriver.Chrome(executable_path= '/Volumes/Work/Face2Face/Xity/streetTeam/Scrappers/chromedriver', options = options)
+
+# %%
+def runAll(url):
+    links_array = []
+    shows_array = []
+    
+    driver.get(url)
+
+    band_name = driver.find_element(By.CLASS_NAME, '_2nzteM_uGca2Jnei9WwoIO').text.strip()
+
+    show_more_button = driver.find_element(By.CLASS_NAME, '_1GaeyllMT79LYiH-6HrT56')
+    show_more_button.click()
+
+    all_events = driver.find_element(By.CLASS_NAME, 'qznXLxZY-XWcgeMFMR1So')
+
+    div = all_events.find_element(By.TAG_NAME, 'div')
+    links_tags = div.find_elements(By.TAG_NAME, 'a')
+
+    for link in links_tags:
+        links_array.append(link.get_property('href'))
+
+    for link in links_array:
+        driver.get(link)
+        
+        venue_div = driver.find_element(By.CLASS_NAME, '_2wsU7P2Nq0F2Ewki3qdVwK')
+        venue_link = venue_div.find_element(By.TAG_NAME, 'a')
+        venue_name = venue_link.text
+
+        if any([x in venue_name.lower() for x in ohmicity_shared.venue_array]): 
+                    pass
+        else:
+                    continue
+
+        show_date = driver.find_element(By.CLASS_NAME, '_1TLOSkbytCU0xipvsRNoDv').text
+        show_time = driver.find_element(By.CLASS_NAME, '_1iK6x88EqsupILFxTvC9ip').text
+        raw_time = show_date + " " + show_time
+
+        this_time = parse(raw_time)
+        date_string = '{:%b %d, %Y %-I:%M%p}'.format(this_time)
+
+        showDict = {}
+        showDict['venue'] = venue_name
+        showDict['band'] = band_name
+        showDict['dateString'] = date_string
+        shows_array.append(showDict)
+
+    shows = {}
+    shows['shows'] = shows_array
+
+    save_path = ohmicity_shared.band_data_path
+    file_name = band_name + '.json'
+    complete_name = os.path.join(save_path, file_name)
+
+    file = open(complete_name, 'w')
+    file.write(json.dumps(shows, indent = 2))
+    file.close()
+    print(f"{band_name} Complete!")
+    
+
+    
+
+# %%
+print("Bands In Town Started")
+
+for url in urls:
+    runAll(url)
+
+driver.quit()
+print("Bands In Town Complete")
+
+
---
+++ Scrappers/Band Scrappers/Scripts/OhmicityShared.py
@@ -0,0 +1,20 @@
+
+class ohmicity_shared:
+
+    venue_array = [
+
+        '210', "blue rooster", 'burger-ish', "river roo", "cage brewing", "cask & ale", "cask and ale", "coconut", "cottonmouth", "daiquiri deck raw bar siesta key", "darwin", "del frisco", "dockside", "drunken clam",
+        "dearborn", "floridian social club", "flute", "gator club", "gilligan", "green iguana", "hideaway", "independent bar st petersburg", "independent bar and cafe tampa", "island time",
+        "joyland", "hatters", "marina jack", "mattison's city grille", "mattison's riverwalk grille", "motorworks", "new world brewery", "o'brien", "pete's place north", "ringside cafe", "ruby's elixir",
+        "sandBar", "hard rock", "sharky", "siesta key oyster bar", "skipper", "st pete side lot", "stottlemyer", "straggler", "tj carney", "tamiami tap", "the castle", "the pub tampa bay", "the tavern at bayboro",
+        "whiskey barrel", "whiskey wings - st peterburg", "toasted monkey", "white elephant pub", "wild rover brewery", 
+    ]
+
+    band_data_path = '/Volumes/Work/Face2Face/Xity/streetTeam/Scrappers/Show Data/Band Data'
+    
+    
+
+    
+    
+    
+    
